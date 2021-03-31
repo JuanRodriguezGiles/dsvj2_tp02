ddddc07bd0c891dbdd78d458f5dbd44d1f681abd
Juan <juanrodriguezgiles@gmail.com>
2021-03-31 20:59:48 -0300

    Implemented basic spawner for planets

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] [Range(1, 10)] private float orbitSpeed;
+    [SerializeField] [Range(0.1f, 1)] private float rotationSpeed;
+    [SerializeField] [Range(1, 100)] public float radius;
+    [SerializeField] private bool sun;
+    private float angle = 0;
+    void Update()
+    {
+        if (sun)
+            return;
+
+        Vector3 v3 = Vector3.zero;
+        angle += orbitSpeed * Time.deltaTime;
+
+        v3.x = radius * Mathf.Cos(angle);
+        v3.z = radius * Mathf.Sin(angle);
+
+        transform.localPosition = v3;
+        transform.Rotate(new Vector3(0, rotationSpeed, 0));
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject planetPrefab;
+    public List<Planet> generatedPlanets = new List<Planet>();
+
+    [Range(1, 15)] public int planetCount = 8;
+    private float distanceBetweenPlanet = 2.0f;
+    void Start()
+    {
+        for (int i = 0; i < planetCount; i++)
+        {
+            float distanceToCenter = 1 + i * distanceBetweenPlanet;
+
+            GameObject newPlanet = Instantiate(planetPrefab, new Vector3(distanceToCenter, 0, 0), Quaternion.identity);
+            Planet planet = newPlanet.GetComponent<Planet>();
+
+            planet.radius = distanceToCenter;
+            generatedPlanets.Add(planet);
+        }
+    }
+}
