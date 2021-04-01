c45adeaca5a0083ff647a21d3c226dca20b2b464
Juan <juanrodriguezgiles@gmail.com>
2021-04-01 10:55:08 -0300

    Added random values to orbitSpeed and rotationSpeed on start

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    [SerializeField] [Range(1, 10)] private float orbitSpeed;
+    [SerializeField] [Range(0.5f, 2)] private float orbitSpeed;
     [SerializeField] [Range(0.1f, 1)] private float rotationSpeed;
     [SerializeField] [Range(1, 100)] public float radius;
     [SerializeField] private bool sun;
     private float angle = 0;
+
+    void Start()
+    {
+        orbitSpeed=Random.Range(0.5f, 1.5f);
+        rotationSpeed = Random.Range(0.1f, 1.0f);
+    }
     void Update()
     {
         if (sun)
