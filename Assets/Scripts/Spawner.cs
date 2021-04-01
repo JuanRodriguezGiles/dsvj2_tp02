41ceb6bcd87e9882968ec03fd0d88ff034e30b5e
Juan <juanrodriguezgiles@gmail.com>
2021-04-01 10:47:06 -0300

    Created Sun and added point light to it

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject planetPrefab;
-    public List<Planet> generatedPlanets = new List<Planet>();
+    private List<Planet> generatedPlanets = new List<Planet>();
 
     [Range(1, 15)] public int planetCount = 8;
     private float distanceBetweenPlanet = 2.0f;
@@ -13,7 +13,7 @@ public class Spawner : MonoBehaviour
     {
         for (int i = 0; i < planetCount; i++)
         {
-            float distanceToCenter = 1 + i * distanceBetweenPlanet;
+            float distanceToCenter = 5 + i * distanceBetweenPlanet;
 
             GameObject newPlanet = Instantiate(planetPrefab, new Vector3(distanceToCenter, 0, 0), Quaternion.identity);
             Planet planet = newPlanet.GetComponent<Planet>();
