4226de022dec07963301bd3ce6a2c0500a4487b1
Juan <juanrodriguezgiles@gmail.com>
2021-04-01 11:27:22 -0300

    Added ship with basic movement

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ship : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 directionVector3 = new Vector3(hor, 0, ver);
+
+        transform.position += directionVector3 * speed * Time.deltaTime;
+    }
+}
+
