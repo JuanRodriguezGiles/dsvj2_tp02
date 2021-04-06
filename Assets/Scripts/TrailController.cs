100a4a1cfa4a2d1d5a800d2d3440b76b9ce78c82
Juan <juanrodriguezgiles@gmail.com>
2021-04-06 20:18:03 -0300

    Updated trail rendered for ship

---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TrailController : MonoBehaviour
+{
+    private TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            trail = GetComponentInParent<TrailRenderer>();
+            trail.enabled = false;
+        }
+        else
+        {
+            trail = GetComponentInParent<TrailRenderer>();
+            trail.enabled = true;
+        }
+    }
+}
